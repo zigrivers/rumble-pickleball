16c5cff3f01706e7d65489aaadd287ca3e3f4128
Ken Allred <zigrivers@gmail.com>
2026-06-13 14:54:08 -0600

    chore(sw): bump cache version to v8 for accessibility release

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 // delete by "everything that isn't the current version".
 const CACHE_PREFIX = "rumble-pickleball-";
 // Bump VERSION on every deploy so clients pick up changes.
-const VERSION = CACHE_PREFIX + "v7";
+const VERSION = CACHE_PREFIX + "v8";
 const SHELL = [
   "./",
   "index.html",
