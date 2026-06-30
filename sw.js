27e49c409fadcfec229b104aa8104c0024d7dd98
Ken Allred <zigrivers@gmail.com>
2026-06-29 18:18:40 -0600

    feat: allow users to customize court names in the Players & Courts setup tab

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 // delete by "everything that isn't the current version".
 const CACHE_PREFIX = "rumble-pickleball-";
 // Bump VERSION on every deploy so clients pick up changes.
-const VERSION = CACHE_PREFIX + "v18";
+const VERSION = CACHE_PREFIX + "v19";
 const SHELL = [
   "./",
   "index.html",
