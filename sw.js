9ef08f62d1511d8f1fdbfa0f7468c41d265c2969
Ken Allred <zigrivers@gmail.com>
2026-06-29 18:26:26 -0600

    feat: display dynamic player experience stats when customizing rounds and auto-calculate recommended mixer defaults

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 // delete by "everything that isn't the current version".
 const CACHE_PREFIX = "rumble-pickleball-";
 // Bump VERSION on every deploy so clients pick up changes.
-const VERSION = CACHE_PREFIX + "v19";
+const VERSION = CACHE_PREFIX + "v20";
 const SHELL = [
   "./",
   "index.html",
